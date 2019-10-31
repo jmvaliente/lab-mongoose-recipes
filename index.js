f3254adf3533874e8382aee317f55521656ffc1f
Chamana <chamana@localhost.localdomain>
2019-10-31 19:10:10 +0100

    done

--- index.js
+++ index.js
@@ -10,3 +10,45 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+  const recipe = {
+    title: 'Huevos con Chorizo',
+    level: 'Amateur Chef',
+    ingredients: [
+      '2 Eggs',
+      '1 Chorizo',
+      '1 salt',
+      '50ml olive oil'
+    ],
+    cuisine: 'Fusion',
+    dishType: 'Dish',
+    image: 'http://www.iaba.com/cookbook/images/recipe_19.png',
+    duration: 10,
+    creator: 'Jose Miguel Brave'
+  }
+  
+  Recipe.create(recipe)
+  
+  .then ((recipe)=>{
+    console.log("Iteration 2 Done")
+    return Recipe.insertMany(data)
+  })
+  .then((recipe)=>{
+    console.log("Iteration 3 Done")
+    return Recipe.updateOne({ title:"Rigatoni alla Genovese" , duration: 100 })
+  })
+  .then((recipe)=>{
+    console.log("Iteration 4 Done")
+    return Recipe.deleteOne({title:"Carrot Cake"})
+
+  })
+  .then((recipe)=>{
+    console.log("Iteration 5 Done")
+    return mongoose.connection.close()
+    
+  })
+  .catch((error)=> console.log("Error"))
+
+
+
+
+
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,8 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const opcionLevel = ["Easy Peasy","Amateur Chef","UltraPro Chef"]
+const opcionDishType = ["Breakfast","Dish","Snack","Drink","Dessert","Other"]
+
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title:{type:String, required:true},
+  level:{type:String, enum:opcionLevel},
+  ingredients:[],
+  cuisine:{type:String, required:true},
+  dishType:{type:String, enum:opcionDishType},
+  image:{type:String, default:'https://images.media-allrecipes.com/images/75131.jpg'},
+  duration:{type:Number, min:0},
+  creator:{type:String},
+  created:{type:Date, default: Date.now}
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
